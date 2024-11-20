e700d573befc6e6b6dd529f7b2caf3100a8e9abe
Jacob Almnes <jacob.almnes@gmail.com>
2024-11-20 10:55:46 +0100

    path fix

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from typing import Dict, Tuple
 
 def get_paths() -> dict:
     #Print directory this is in (full path)
-    _project = Path('/cluster') / 'work' / 'jacobaal' / 'pers-pred'
+    _project = Path('/cluster') / 'work' / 'jacobaal' / 'prosjekt_oppgave'
     #this is giga hacky
     _data = _project / 'data'
     _raw = _data / 'raw'
